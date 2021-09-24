32d9e6c06578dab2ed3e1d7b8253f578d1e33923
yeison378 <yeisonalexis1@outlook.es>
2021-09-24 15:58:10 -0500

    corredores- indicador

--- src/main/java/com/example/taller_2/Controller.java
+++ src/main/java/com/example/taller_2/Controller.java
@@ -6,6 +6,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -36,5 +38,42 @@ public class Controller implements Initializable {
     Stage stage = (Stage) minimizar.getScene().getWindow();
     stage.setIconified(true);
 
+    }
+
+    @FXML
+    private ImageView jugador,jugador1,jugador2,jugador3,jugador4;
+
+    @FXML
+    private Button mover,aleatorio;
+
+    @FXML
+    private TextField texto;
+
+    @FXML
+    void moverEnX(ActionEvent event) {
+        jugador.setX(Double.parseDouble(texto.getText()));
+
+
+        jugador1.setX((int)(Math.random()*100+1)+(jugador1.getX()));
+
+        System.out.println(jugador.getX());
+    }
+
+    @FXML
+    void mov(ActionEvent event) {
+
+
+        if(jugador.getX()<(Double)960.0)jugador.setX((int)(Math.random()*40+1)+(jugador.getX()));
+        if(jugador1.getX()<(Double)960.0)jugador1.setX((int)(Math.random()*40+1)+(jugador1.getX()));
+        if(jugador2.getX()<(Double)960.0)jugador2.setX((int)(Math.random()*40+1)+(jugador2.getX()));
+        if(jugador3.getX()<(Double)960.0)jugador3.setX((int)(Math.random()*40+1)+(jugador3.getX()));
+        if(jugador4.getX()<(Double)960.0)jugador4.setX((int)(Math.random()*40+1)+(jugador4.getX()));
+
+//        jugador.setX(960);
+//        jugador1.setX(960);
+
+
+//        System.out.println(jugador.getX());
+//        System.out.println(jugador1.getX());
     }
 }
